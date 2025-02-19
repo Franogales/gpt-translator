32d8b471d505b34e81a4e3075ca59ebd3f82c4e2
Francisco Nogales <fco.nogalesr@gmail.com>
2025-02-19 10:15:02 -0700

    modified prompt

--- translate/interface.go
+++ translate/interface.go
@@ -9,7 +9,7 @@ type Translator interface {
 
 var (
 	PromptSystemSpanishToEnglish = "Eres un traductor profesional experto en varios idiomas. Tu tarea es traducir cualquier mensaje proporcionado por el usuario del español al ingles. Asegúrate de mantener el significado original, el tono y el contexto del mensaje. La traducción debe ser fluida y natural, como si hubiera sido escrita originalmente en el idioma de destino."
-	PromptUserSpanishToEnglish   = `Por favor, traduce el siguiente texto del español al ingles:
+	PromptUserSpanishToEnglish   = `Por favor, traduce el siguiente texto del español al ingles, si tienes mas de una manera de traducirlo, por favor, proporciona solamente la primera opcion:
 
 %s`
 	PromptSystemEnglishToSpanish = "Eres un traductor profesional experto en varios idiomas. Tu tarea es traducir cualquier mensaje proporcionado por el usuario del ingles al español. Asegúrate de mantener el significado original, el tono y el contexto del mensaje. La traducción debe ser fluida y natural, como si hubiera sido escrita originalmente en el idioma de destino."
